75a3b7cbd45f8eb80ebda937d3831958fe42677a
Sam Feighery <sfeighery@gmail.com>
2024-05-23 12:34:21 +0100

    inital commit

---
+++ dags/hello.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_augs = {
+    'owner': 'SamFeighery',
+    'start_date': datetime(2024, 5, 23),
+    'catchup': False
+}
+
+dag = DAG(
+    'hello_world',
+    default_args= default_augs,
+    schedule=timedelta(days=1)
+)
+
+t1 = BashOperator(
+    task_id = 'hello_world',
+    bash_command='echo "Hello World"',
+    dag = dag
+)
+
+t2 = BashOperator(
+    task_id = 'hello_Sam',
+    bash_command='echo "Hello Sam"',
+    dag = dag
+)
+
+t1 >> t2
